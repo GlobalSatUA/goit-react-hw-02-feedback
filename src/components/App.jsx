6e7b8bab7cd4f6236dbcb0e5f8838153745965d2
GlobalSatUA <alex.trenbach@gmail.com>
2023-06-10 12:05:35 +0300

    second

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,50 @@
+
+import React, { Component } from 'react';
 import Feedback from './Feedback/Feedback';
 
-export const App = () => {
-  return (
-      <div>
-        <Feedback />
-      </div>
-  );
-};
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
 
+  handleButtonClick = (feedbackType) => {
+    this.setState((prevState) => ({
+      [feedbackType]: prevState[feedbackType] + 1
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    return totalFeedback > 0 ? Math.round((good / totalFeedback) * 100) : 0;
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <div>
+        <Feedback
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          totalFeedback={totalFeedback}
+          positivePercentage={positivePercentage}
+          onLeaveFeedback={this.handleButtonClick}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
 
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,63 +1,53 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import FeedbackOptions from '../FeedbackOptions/FeedbackOptions';
 import Statistics from '../Statistics/Statistics';
 import Section from '../Section/Section';
 import Notification from '../Notification/Notification';
 
-class Feedback extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  };
+const Feedback = ({
+  good,
+  neutral,
+  bad,
+  totalFeedback,
+  positivePercentage,
+  onLeaveFeedback
+}) => {
+  const hasFeedback = totalFeedback > 0;
 
-  handleButtonClick = (feedbackType) => {
-    this.setState((prevState) => ({
-      [feedbackType]: prevState[feedbackType] + 1
-    }));
-  };
-
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const totalFeedback = this.countTotalFeedback();
-    return totalFeedback > 0 ? Math.round((good / totalFeedback) * 100) : 0;
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const totalFeedback = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
-    const hasFeedback = totalFeedback > 0;
-
-    return (
-      <div style={{ marginLeft: '20px' }}>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.handleButtonClick}
+  return (
+    <div style={{ marginLeft: '20px' }}>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={onLeaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        {hasFeedback ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={positivePercentage}
           />
-        </Section>
-        <Section title="Statistics">
-          {hasFeedback ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={totalFeedback}
-              positivePercentage={positivePercentage}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
+};
+
+Feedback.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  totalFeedback: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired
+};
 
 export default Feedback;
+
